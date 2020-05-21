751990e676bc2773784905ef6e4332e4b8504845
Amelia Davis <ameliaaltman22@gmail.com>
2020-05-20 21:15:40 -0600

    deploy react to heroku

---
+++ src/components/tableRow.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Table } from "reactstrap";
+
+
+const TableRow = props => {
+      
+            return (
+                <tbody>
+                      <tr key={props.email}>
+                        <td>{props.first}</td>
+                        <td>{props.last}</td>
+                        <td>{props.email}</td>
+                        <td>{props.dob}</td>
+                        <td>{props.cell}</td>
+                        <td>{props.state}</td>
+                    </tr> 
+                </tbody>
+            );
+        }
+  
+  export default TableRow;   
